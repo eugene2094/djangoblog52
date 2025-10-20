75cbe5ce04228aaeb9e01e700f3b2d5a021ec318
eugene2094 <70202369+eugene2094@users.noreply.github.com>
2025-10-20 20:21:10 +0300

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -144,3 +144,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/media/'
 
 CART_SESSION_ID = 'cart'
+
